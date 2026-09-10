0698ff4c573741529d6761fb2c4d5a9c4f180ca8
Sumit Kumar <sk1183189018@gmail.com>
2026-09-10 23:01:59 +0530

    Initialize Electron app with main window setup

---
+++ src/main.ts
@@ -0,0 +1,39 @@
+import { app, BrowserWindow } from "electron";
+import path from "node:path";
+
+function createWindow(): void {
+  const win = new BrowserWindow({
+    width: 1440,
+    height: 900,
+    minWidth: 1000,
+    minHeight: 650,
+    backgroundColor: "#0b0f19",
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  if (!app.isPackaged) {
+    win.loadURL("http://localhost:5173");
+  } else {
+    win.loadFile(path.join(__dirname, "../dist/index.html"));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
